f7df94d1ebc1327636ba8fc5b2de67ddd5c0429a
maod4github <maod4email@sina.com>
2016-02-27 09:53:04 +0800

    /views/home.html -> /views/index.html

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var UserController = require('./controllers/front/UserController.js');
 
 router.get('/', function (req, res, next) {
-  res.render('home');
+  res.render('index');
 });
 
 router.post('/front/user/find', UserController.find);
